5c50e4acb33dcef64db82157b40b6c4c120c0bf7
Wali Temuri <walitemuri@gmail.com>
2026-08-27 04:20:59 -0400

    Support multi-machine Forge workers

--- worker/generated/forge.grpc.pb.cc
+++ worker/generated/forge.grpc.pb.cc
@@ -15,11 +15,10 @@
 #include <grpcpp/support/method_handler.h>
 #include <grpcpp/impl/rpc_service_method.h>
 #include <grpcpp/support/server_callback.h>
-#include <grpcpp/impl/server_callback_handlers.h>
+#include <grpcpp/impl/codegen/server_callback_handlers.h>
 #include <grpcpp/server_context.h>
 #include <grpcpp/impl/service_type.h>
 #include <grpcpp/support/sync_stream.h>
-#include <grpcpp/ports_def.inc>
 namespace forge {
 namespace v1 {
 
@@ -162,5 +161,4 @@ ForgeController::Service::~Service() {
 
 }  // namespace forge
 }  // namespace v1
-#include <grpcpp/ports_undef.inc>
 
