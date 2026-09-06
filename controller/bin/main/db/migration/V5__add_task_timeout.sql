99a45d58877d770fff2540cd94be1e89855f2976
Wali Temuri <walitemuri@gmail.com>
2026-09-06 08:51:02 -0400

    Ignore generated controller output

--- controller/bin/main/db/migration/V5__add_task_timeout.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE forge_tasks
-    ADD COLUMN timeout_seconds INTEGER NOT NULL DEFAULT 0;
-
-ALTER TABLE forge_tasks
-    ADD CONSTRAINT forge_tasks_timeout_seconds_check
-    CHECK (
-        timeout_seconds >= 0
-        AND timeout_seconds <= 86400
-    );
